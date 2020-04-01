efccb9982dc8053d5968ed032f1315d2fc398339
saskiakuusk <saskiakuusk@users.noreply.github.com>
2020-04-01 16:16:03 +0300

    Klassid ja rassid

--- src/klassid/KlassSodalane.java
+++ src/klassid/KlassSodalane.java
@@ -1,7 +1,24 @@
 package klassid;
 
+import java.util.List;
+
 public class KlassSodalane extends Klass {
+    private List<String> relvad;
+
     public KlassSodalane(String klass) {
         super(klass);
+    }
+
+    public String valiRelv(){
+        relvad.add("Mõõk");
+        relvad.add("Kahekäemõõk");
+        relvad.add("Kirves");
+        relvad.add("Pistoda");
+        return super.valiRelv(relvad);
+    }
+    @Override
+    public String toString() {
+        valiRelv();
+        return super.toString() + "Relvaks on " + valiRelv() + ". ";
     }
 }
--- src/klassid/KlassVibukutt.java
+++ src/klassid/KlassVibukutt.java
@@ -1,7 +1,24 @@
 package klassid;
 
+import java.util.List;
+
 public class KlassVibukutt extends Klass {
+    private List<String> relvad;
+
     public KlassVibukutt(String klass) {
         super(klass);
+    }
+
+    public String valiRelv(){
+        relvad.add("Vibu");
+        relvad.add("Ammu");
+        relvad.add("Viskenuga");
+        relvad.add("Oda");
+        return super.valiRelv(relvad);
+    }
+    @Override
+    public String toString() {
+        valiRelv();
+        return super.toString() + "Relvaks on " + valiRelv() + ". ";
     }
 }
